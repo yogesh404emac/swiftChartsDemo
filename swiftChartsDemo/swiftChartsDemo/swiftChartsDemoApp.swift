6f240d541304d869d9f9d923a320846f7119a640
“yogesh404emac” <yogesh404emac@gmail.com>
2024-01-14 15:11:15 +0530

    initial empty project

---
+++ swiftChartsDemo/swiftChartsDemo/swiftChartsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftChartsDemoApp.swift
+//  swiftChartsDemo
+//
+//  Created by Yogesh on 14/01/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftChartsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
