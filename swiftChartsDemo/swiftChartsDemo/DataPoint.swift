9a142ab739141e8405b536fcdb060cad3ec1f49b
“yogesh404emac” <yogesh404emac@gmail.com>
2024-01-14 15:40:40 +0530

    added StateData strcut data array in the Datapoint

---
+++ swiftChartsDemo/swiftChartsDemo/DataPoint.swift
@@ -0,0 +1,23 @@
+//
+//  DataPoint.swift
+//  swiftChartsDemo
+//
+//  Created by Yogesh on 14/01/24.
+//
+
+import Foundation
+
+struct StateData {
+    static var data: [StateInfo] = [
+        StateInfo(IDState: "04000US01", state: "Alabama", IDYear: 2021, year: "2021", population: 2997, slugState: "alabama"),
+        StateInfo(IDState: "04000US02", state: "Alaska", IDYear: 2021, year: "2021", population: 5358, slugState: "alaska"),
+        StateInfo(IDState: "04000US04", state: "Arizona", IDYear: 2021, year: "2021", population: 6079, slugState: "arizona"),
+        StateInfo(IDState: "04000US04", state: "Arizona", IDYear: 2022, year: "2021", population: 1079, slugState: "arizona"),
+        StateInfo(IDState: "04000US05", state: "Arkansas", IDYear: 2021, year: "2021", population: 3006, slugState: "arkansas"),
+        StateInfo(IDState: "04000US06", state: "California", IDYear: 2021, year: "2021", population: 7945, slugState: "california"),
+        StateInfo(IDState: "04000US08", state: "Colorado", IDYear: 2021, year: "2021", population: 1723, slugState: "colorado"),
+        StateInfo(IDState: "04000US09", state: "Connecticut", IDYear: 2021, year: "2021", population: 2605, slugState: "connecticut"),
+        StateInfo(IDState: "04000US10", state: "Delaware", IDYear: 2021, year: "2021", population: 5818, slugState: "delaware"),
+        StateInfo(IDState: "04000US11", state: "District of Columbia", IDYear: 2023, year: "2021", population: 683, slugState: "district-of-columbia")
+    ]
+}
