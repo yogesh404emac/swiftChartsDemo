e109c58a3895451028501d8ca9d20d13617779f1
“yogesh404emac” <yogesh404emac@gmail.com>
2024-01-14 15:29:47 +0530

    added StateInfo struct

---
+++ swiftChartsDemo/swiftChartsDemo/StateInfo.swift
@@ -0,0 +1,13 @@
+//
+//  StateInfo.swift
+//  swiftChartsDemo
+//
+//  Created by Yogesh on 14/01/24.
+//
+
+import Foundation
+
+//We can use this struct after Api add we can use it for Codable protocol new we use static data with StateData
+struct StateInfo{
+    
+}
