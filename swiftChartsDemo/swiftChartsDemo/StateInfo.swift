f3efc2012f4f46ff8da4ddd11b842304518a0e4d
“yogesh404emac” <yogesh404emac@gmail.com>
2024-01-14 16:09:21 +0530

    updated StateInfo with codable protocol with Datapoint

--- swiftChartsDemo/swiftChartsDemo/StateInfo.swift
+++ swiftChartsDemo/swiftChartsDemo/StateInfo.swift
@@ -8,6 +8,20 @@
 import Foundation
 
 //We can use this struct after Api add we can use it for Codable protocol new we use static data with StateData
-struct StateInfo{
-    
+struct StateInfo: Codable {
+    let IDState: String
+    let state: String
+    let IDYear: Int
+    let year: String
+    let population: Int
+    let slugState: String
+
+    enum CodingKeys: String, CodingKey {
+        case IDState = "ID State"
+        case state = "State"
+        case IDYear = "ID Year"
+        case year = "Year"
+        case population = "Population"
+        case slugState = "Slug State"
+    }
 }
